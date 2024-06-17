594079a4153d9314e004b2cf481ed40ec5d9e39a
Katsiaryna <katsiaryna.n@gmail.com>
2024-06-17 14:08:47 +0300

    feat: add year filter

---
+++ src/pages/Home/Year/Year.tsx
@@ -0,0 +1,92 @@
+import { ChangeEventHandler, memo } from 'react';
+import { SetURLSearchParams } from 'react-router-dom';
+
+type OptionsListProps = {
+  range: [number, number];
+};
+
+const OptionsList = ({ range }: OptionsListProps) => {
+  const years = range[1] - range[0] + 1;
+  const optionsList = new Array(years).fill(range[1]);
+
+  return (
+    <>
+      <option value="">--</option>
+      {optionsList.map((el, i) => (
+        <option key={i} value={el - i}>
+          {el - i}
+        </option>
+      ))}
+    </>
+  );
+};
+
+type YearItemProps = {
+  label: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+  range: [number, number];
+};
+
+const YearItem = ({ label, value, onChange, range }: YearItemProps) => {
+  return (
+    <label>
+      {' '}
+      {label}
+      <select value={value} onChange={onChange}>
+        <OptionsList range={range} />
+      </select>
+    </label>
+  );
+};
+
+type Props = {
+  years: string;
+  changeQuery: SetURLSearchParams;
+};
+
+function Year({ years, changeQuery }: Props) {
+  console.log('render Year');
+
+  const lastYear = new Date().getFullYear() + 1;
+  const firstYear = 1890;
+
+  const currentYears = years.split('-');
+
+  const startYear = currentYears[0];
+  const endYear = currentYears[1];
+
+  const setStartYear: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    changeQuery((query) => {
+      query.set('year', `${e.target.value}-${endYear || lastYear}`);
+      return query;
+    });
+  };
+
+  const setEndYear: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    changeQuery((query) => {
+      query.set('year', `${startYear || firstYear}-${e.target.value}`);
+      return query;
+    });
+  };
+
+  return (
+    <div>
+      Год выпуска
+      <YearItem
+        label="c"
+        value={startYear}
+        onChange={setStartYear}
+        range={[firstYear, +endYear || lastYear]}
+      />
+      <YearItem
+        label="по"
+        value={endYear}
+        onChange={setEndYear}
+        range={[+startYear || firstYear, lastYear]}
+      />
+    </div>
+  );
+}
+const MemoYear = memo(Year);
+export default MemoYear;
