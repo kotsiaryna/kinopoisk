83c9b94d3b46afefe7dae01ed3e45c519dfcfa08
Katsiaryna <katsiaryna.n@gmail.com>
2024-06-19 10:23:50 +0300

    feat: add filters to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,51 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useAppDispatch } from '../../store/hooks';
 import { fetchAllFilms } from '../../store/features/filmsSlice';
 import FilmList from './FilmList.tsx/FilmList';
 import Pagination from './Pagination/Pagination';
 import LimitPerPage from './LimitPerPage/LimitPerPage';
 import { useSearchParams } from 'react-router-dom';
+import Search from './Search/Search';
+import Year from './Year/Year';
+import Age from './Age/Age';
+import Country from './Country/Country';
 
 function Home() {
+  console.log('render Home');
   const [query, setQuery] = useSearchParams();
-  const currentPage = query.get('page') || '1';
-  const currentLimit = query.get('limit') || '10';
+
+  const page = query.get('page') || '1';
+  const limit = query.get('limit') || '10';
+  const search = query.get('search') || '';
+  const year = query.get('year') || '-';
+  const ageRating = query.getAll('ageRating') || [];
+  const country = query.get('country') || '';
 
   const dispatch = useAppDispatch();
+  const changeQuery = useCallback(setQuery, []);
 
   useEffect(() => {
-    dispatch(fetchAllFilms({ page: currentPage, limit: currentLimit }));
-  }, [currentPage, currentLimit]);
+    dispatch(
+      fetchAllFilms({
+        page,
+        limit,
+        search,
+        year,
+        ageRating,
+        country,
+      }),
+    );
+  }, [page, limit, search, year, ageRating, country]);
 
   return (
     <>
       <div>FILMS</div>
       <Pagination query={query} changeQuery={setQuery} />
-      <LimitPerPage query={query} changeQuery={setQuery} />
+      <Search search={search} changeQuery={changeQuery} />
+      <Year years={year} changeQuery={changeQuery} />
+      <Age age={ageRating} changeQuery={changeQuery} />
+      <Country country={country} changeQuery={changeQuery} />
+      <LimitPerPage limit={limit} changeQuery={changeQuery} />
       <FilmList />
     </>
   );
